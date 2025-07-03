8e559180735a45392dd545e51ce4618115e274d4
sjs1729 <87244944+sjs1729@users.noreply.github.com>
2025-07-04 03:03:42 +0530

    Update 1_Standings.py

--- pages/1_Standings.py
+++ pages/1_Standings.py
@@ -23,7 +23,13 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
+# Hide Streamlit menu and footer
+hide_streamlit_style = """
+        <style>
+        .stToolbarActions {display: none !important;}
+        </style>
+        """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 def get_tournament_status():
     df = Load_MatchResults()
