194f3566450e56392bedb762b273dd8111b1da74
sjs1729 <87244944+sjs1729@users.noreply.github.com>
2025-07-04 03:02:53 +0530

    Update EGVTennis.py

--- EGVTennis.py
+++ EGVTennis.py
@@ -22,7 +22,13 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
+# Hide Streamlit menu and footer
+hide_streamlit_style = """
+        <style>
+        .stToolbarActions {display: none !important;}
+        </style>
+        """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.markdown("""
     <style>
