8a504defed792cb78b7c8ca126f693604b2c47d3
sjs1729 <87244944+sjs1729@users.noreply.github.com>
2025-09-14 00:43:18 +0530

    Update EGVTennis.py

--- EGVTennis.py
+++ EGVTennis.py
@@ -154,9 +154,9 @@ st.markdown(f"""
 
 
 
-st.sidebar.markdown("<BR>",unsafe_allow_html=True)
-image_html = rounded_image_html("Sponsor.jpg", 300)
-st.sidebar.markdown(image_html, unsafe_allow_html=True)
+#st.sidebar.markdown("<BR>",unsafe_allow_html=True)
+#image_html = rounded_image_html("Sponsor.jpg", 300)
+#st.sidebar.markdown(image_html, unsafe_allow_html=True)
 
 
 
